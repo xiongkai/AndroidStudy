2d7c2988718edd81edc9e8a4fd6b27b90beb31b8
scott <291309079@qq.com>
2020-12-21 19:29:49 +0800

    update

--- OpenSource/src/main/java/com/test/opensource/MainActivity.java
+++ OpenSource/src/main/java/com/test/opensource/MainActivity.java
@@ -7,6 +7,15 @@ import com.squareup.leakcanary.LeakCanary;
 import org.greenrobot.eventbus.EventBus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,12 +25,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
         LeakCanary.install(getApplication());
+        //
         EventBus.getDefault().register(this);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://postman-echo.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
+        //
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://www.baidu.com").build();
+            Call call = client.newCall(request);
+            Response response = call.execute();
+        }catch (Exception ex){}
+        //
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://postman-echo.com/")
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        //
+        Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override public void subscribe(ObservableEmitter<Object> emitter) throws Exception { }
+        }).map(new Function<Object, Object>() {
+            @Override public Object apply(Object o) throws Exception { return null; }
+        }).subscribeOn(Schedulers.io()).observeOn(Schedulers.io()).subscribe();
     }
 }
